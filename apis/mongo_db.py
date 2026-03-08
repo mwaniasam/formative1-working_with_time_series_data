0ad98811db31022c5487e5ffdfb9531b92485db6
Jok Maker <165774490+JokMaker@users.noreply.github.com>
2026-03-08 20:58:13 +0200

    Delete apis directory

--- apis/mongo_db.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-
-# Connect to local MongoDB
-client = MongoClient("mongodb://localhost:27017")
-
-# Access database and collection
-db = client["energy_db"]
-collection = db["energy_hourly"]
